62962c2b056cd80637a8c20604f94b3da4079a6c
Inga <inga.s6699@gmail.com>
2021-07-22 14:57:26 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+const getQuoteButton = document.querySelector("#get-quote-button");
+const quoteText = document.querySelector("#quote-text");
+const quoteAuthor = document.querySelector("#quote-text");
