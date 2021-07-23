b0b80f34621d32319b613ceac70076069b2d3525
Inga <inga.s6699@gmail.com>
2021-07-23 10:44:44 +0200

    fix: restructured code, need to fix

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+let arr = [];
+
 const getQuoteButton = document.querySelector("#get-quote-button");
-const quoteText = document.querySelector("#quote-text");
-const quoteAuthor = document.querySelector("#quote-text");
+const baseUrl = "https://quote-garden.herokuapp.com/api/v3/quotes/random"
+
+getQuoteButton.addEventListener("click", getQuote);
+
+class TextQuote {
+    constructor(text, author) {
+        this.qText = text;
+        this.qAuthor = author;
+    }
+}
+
+function getQuote() {
+    fetch(baseUrl)
+      .then(response => response.json())
+      .then(data => {
+          arr.push(data);
+          console.log(data);
+          let newTextQuote = new TextQuote(
+            arr.data[0].quoteText,
+            arr.data[0].quoteAuthor
+          )
+      });
+  createQuote();
+}
+
+function createQuote(quote) {
+    const createBlockquote = document.createElement("blockquote");
+    createBlockquote.innerText = quote.qText;
+    createBlockquote.innerText = quote.qAuthor;
+}
